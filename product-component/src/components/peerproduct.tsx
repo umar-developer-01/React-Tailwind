b7e87256168837c8f0669e93b99dbd4038b18668
umar-developer-01 <umarkhandev9@gmail.com>
2024-03-03 21:25:52 +0530

    Change in the peer product

--- product-component/src/components/peerproduct.tsx
+++ product-component/src/components/peerproduct.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Jacket from "../assets/jacket.jpg";
+import RenderStars from "./renderStars";
 
 interface PeerProductProps { }
 
@@ -13,16 +14,7 @@ const PeerProduct: React.FC<PeerProductProps> = () => {
         setSelectedSize(size);
     };
 
-    // render stars
-    const renderStars = () => {
-        const stars = Array.from({ length: 5 }, (_, index) => (
-            <svg key={index} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-6 h-6 fill-current ${index < rating ? 'text-teal-500' : 'text-gray-300'}`}>
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-            </svg>
-        ));
 
-        return stars;
-    };
 
 
     return (
@@ -30,7 +22,7 @@ const PeerProduct: React.FC<PeerProductProps> = () => {
             <div className="bg-white w-96 flex flex-col gap-2 shadow-xl rounded-lg overflow-hidden border">
                 <img src={Jacket} alt="Jacket" className="h-[400px] w-96 object-cover object-center" />
                 <div className="p-6">
-                    <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+                    <div className="flex items-baseline mt-2 mb-6 pb-6 border-b border-slate-200">
                         <div className="space-x-2 flex text-sm">
                             {['xs', 's', 'm', 'l', 'xl'].map((size) => (
                                 <label key={size}>
@@ -55,7 +47,7 @@ const PeerProduct: React.FC<PeerProductProps> = () => {
                         <span className="text-gray-600 text-sm"> 41% OFF</span>
                     </div>
                     <div className="mt-2 flex items-center">
-                        {renderStars()}
+                        <RenderStars rating={rating} />
                         <span className="ml-2 text-gray-600 text-sm">  44 reviews</span>
                     </div>
                 </div>
