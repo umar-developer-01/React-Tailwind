957077ad75baf3d57fa830b79a3579cf9fb1308f
umar-developer-01 <umarkhandev9@gmail.com>
2024-03-09 21:34:46 +0530

    Added shadow and depth

--- product-component/src/components/peerproduct.tsx
+++ product-component/src/components/peerproduct.tsx
@@ -18,42 +18,44 @@ const PeerProduct: React.FC<PeerProductProps> = () => {
 
 
     return (
-        <div className="flex justify-center items-center h-screen antialiased text-gray-900">
-            <div className="bg-white w-96 flex flex-col gap-2 shadow-xl rounded-lg overflow-hidden border">
-                <img src={Jacket} alt="Jacket" className="h-[400px] w-96 object-cover object-center" />
-                <div className="p-6 pt-2">
+        <div className="bg-gray-100 flex justify-center items-center h-screen antialiased text-gray-900">
+            <div className="w-96 flex flex-col gap-2  rounded-lg overflow-hidden">
+                <img src={Jacket} alt="Jacket" className="h-[400px] w-96 object-cover object-center shadow-md" />
+                <div className="relative px-4 -mt-6">
+                    <div className="bg-white p-6 pt-2 rounded-lg shadow-lg">
 
-                    <div className="mt-1 flex items-baseline mb-4 pb-6 border-b border-slate-200">
-                        <div className="space-x-2 flex text-sm">
-                            {['xs', 's', 'm', 'l', 'xl'].map((size) => (
-                                <label key={size}>
-                                    <input
-                                        className="sr-only peer"
-                                        name="size"
-                                        type="radio"
-                                        value={size}
-                                        checked={selectedSize === size}
-                                        onChange={() => handleSizeChange(size)}
-                                    />
-                                    <div className={`w-9 h-9 rounded-lg uppercase flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white`}>
-                                        {size}
-                                    </div>
-                                </label>
-                            ))}
+                        <div className="mt-1 flex items-baseline mb-4 pb-6">
+                            <div className="space-x-2 flex text-sm">
+                                {['xs', 's', 'm', 'l', 'xl'].map((size) => (
+                                    <label key={size}>
+                                        <input
+                                            className="sr-only peer"
+                                            name="size"
+                                            type="radio"
+                                            value={size}
+                                            checked={selectedSize === size}
+                                            onChange={() => handleSizeChange(size)}
+                                        />
+                                        <div className={`w-9 h-9 rounded-lg uppercase flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white`}>
+                                            {size}
+                                        </div>
+                                    </label>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                    <div className="mb-1 flex items-baseline mt-1">
-                        <span className="bg-green-500 text-white text-sx px-2 py-1 inline-block rounded-md">New</span>
-                        <h4 className="ml-2 font-semibold text-lg leading-tight truncate">Louis Philippe</h4>
-                    </div>
+                        <div className="mb-1 flex items-baseline mt-1">
+                            <span className="bg-green-500 text-white text-sx px-2 py-1 inline-block rounded-md">New</span>
+                            <h4 className="ml-2 font-semibold text-lg leading-tight truncate">Louis Philippe</h4>
+                        </div>
 
-                    <div className="mt-2">
-                        $2,949.00 &bull;
-                        <span className="text-gray-600 text-sm"> 41% OFF</span>
-                    </div>
-                    <div className="mt-2 flex items-center">
-                        <RenderStars rating={rating} />
-                        <span className="ml-2 text-gray-600 text-sm">  44 reviews</span>
+                        <div className="mt-2">
+                            $2,949.00 &bull;
+                            <span className="text-gray-600 text-sm"> 41% OFF</span>
+                        </div>
+                        <div className="mt-2 flex items-center">
+                            <RenderStars rating={rating} />
+                            <span className="ml-2 text-gray-600 text-sm">  44 reviews</span>
+                        </div>
                     </div>
                 </div>
             </div>
